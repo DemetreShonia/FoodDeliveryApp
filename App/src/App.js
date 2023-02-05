e85f1e7f6016c4147c165aa77f7abb54e9c96fce
DemetreShonia <shonka.demeloper@gmail.com>
2023-02-05 12:26:21 +0400

    Modal Done

--- App/src/App.js
+++ App/src/App.js
@@ -1,10 +1,11 @@
 import Header from "./components/Layout/Header";
 import { Fragment } from "react";
 import Meals from "./components/Meals/Meals.js";
-
+import Cart from "./components/Cart/Cart";
 function App() {
   return (
     <Fragment>
+      <Cart />
       <Header />
       <main>
         <Meals />
---
+++ App/src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,23 @@
+import classes from "./MealItemForm.module.css";
+import Input from "../../UI/Input";
+
+const MealItemForm = (props) => {
+  return (
+    <form className={classes.form}>
+      <Input
+        label="Amount"
+        input={{
+          id: "amount_" + props.id,
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+        // this are built in types and will make sense...
+      />
+      <button>+ Add</button>
+    </form>
+  );
+};
+export default MealItemForm;
